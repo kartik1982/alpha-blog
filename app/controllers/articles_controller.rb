d1d536b0eacff56ee7c9456ffae6d017f97e185a
Kartik Aiyar <kartik.aiyar82@gmail.com>
2018-09-24 20:02:44 -0400

    Add Edit record and show list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article= Article.new
   end
@@ -12,9 +15,21 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+  def edit 
+  @article = Article.find(params[:id])
+  end
   def show
     @article = Article.find(params[:id])  
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else 
+      reder 'edit'
+    end
   end
   
   private 
