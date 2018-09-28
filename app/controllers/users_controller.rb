f5ed7f515c58cada48a5f5d60be7a43bc44d3757
Kartik Aiyar <kartik.aiyar82@gmail.com>
2018-09-27 21:01:55 -0400

    Added login and logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-
+ before_action :set_user, only: [:edit, :update, :show]
+  def index
+#     @users= User.all
+    @users = User.paginate(page: params[:page], per_page: 2)
+  end
   def new
     @user = User.new
   end
@@ -15,11 +19,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update (user_params)
       flash[:success] = "Your account updated successfully"
       redirect_to articles_path
@@ -29,10 +31,14 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user = User.find(params[:id])
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
   
   def user_params
   params.require(:user).permit(:username, :email, :password)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
