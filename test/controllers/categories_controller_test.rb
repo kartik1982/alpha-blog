d2272b854deb7f9bcba7372e88f824327899a796
Kartik Aiyar <kartik.aiyar82@gmail.com>
2018-10-02 16:07:17 -0400

    Added Create category

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
 def setup  
   @category = Category.create(name: "sports")
+  @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true )
 end
 
 test "should get category index" do
@@ -9,6 +10,7 @@ test "should get category index" do
   assert_response :success
 end
 test "should get new" do
+  session[:user_id] = @user.id
   get :new
   assert_response :success
 end
@@ -16,6 +18,13 @@ end
 test "should get show" do
   get(:show, {'id' => @category.id})
   assert_response :success
+end
+
+test "should redirect create when admin not logged in" do
+  assert_no_difference 'Category.count' do
+    post :create, category: {name: "sports"}
+  end
+  assert_redirected_to categories_path
 end
 
 end
